6e4254aabe7b3ced40ceb1f0ed28e29ccb87f6c7
Ghodss1 <smarika121stha@gmail.com>
2025-11-06 12:44:05 -0600

    Initial commit - undergrad project

---
+++ Module.py
@@ -0,0 +1,17 @@
+def add(x, y):
+    max_len = 8
+
+    result = ''
+    Carry = 0
+
+    for i in range(max_len - 1, -1, -1):
+        r = Carry 
+        r += 1 if x[i] == '1' else 0
+        r += 1 if y[i] == '1' else 0
+
+        result = ('1' if r % 2 == 1 else '0') + result
+        Carry = 0 if r < 2 else 1
+
+    if Carry != 0: result = '1' + result
+
+    return result.zfill(max_len)
---
+++ Run.py
@@ -0,0 +1,26 @@
+import Input
+import Conversion
+import Module
+
+while True:
+    Input.input_check_first()
+    Input.input_check_second()
+    print("First Number               : ", Input.num1)
+    print("Second Number              : ", Input.num2)
+    bin1 = Conversion.decimal_to_binary(Input.num1)
+    bin2 = Conversion.decimal_to_binary(Input.num2)
+    print("First Number in Binary is  : ", bin1)
+    print("Second Number in Binary is : ", bin2)
+    binary_add = Module.add(bin1, bin2)
+
+    if len(binary_add) <= 8:
+        print("Sum of numbers in Binary   : ", binary_add)
+        dec1 = Conversion.binary_to_decimal(binary_add)
+        print('Sum of numbers in Decimal   : ', dec1)
+    else:
+        print('This is only 8-bit adder and cannot show value more than 8-bit binary')
+    choice = input("Do you want to add more? If yes enter y but if no enter n to complete the program: ")
+    if choice.lower() == "y":
+        print("!!!!!!!Peforming Addition Again!!!!!!!!")
+    else:
+        break
